ca1c116c3134026965f73a0f5fc668fce44b583a
Dusk Banks <me@bb010g.com>
2024-05-27 17:27:33 -0700

    nixos/squeezelite: expose option `package`
    Topic: pkgs/squeezelite
    Branches: master
    Testing-branches: nixos-unstable, master
    Uploader: bb010g

--- nixos/modules/services/audio/squeezelite.nix
+++ nixos/modules/services/audio/squeezelite.nix
@@ -9,22 +9,16 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
+    mkMerge
     mkOption
+    mkPackageOption
     optionalString
     types
     ;
 
   dataDir = "/var/lib/squeezelite";
   cfg = config.services.squeezelite;
-  pkg =
-    if cfg.pulseAudio then
-      pkgs.squeezelite.override {
-        audioBackend = "pulse";
-        pulseSupport = null;
-      }
-    else
-      pkgs.squeezelite;
-  bin = pkg + "/bin/${pkg.meta.mainProgram}";
+  bin = cfg.finalPackage + "/bin/${cfg.finalPackage.meta.mainProgram}";
 in
 {
 
@@ -32,6 +26,12 @@ in
 
   options.services.squeezelite = {
     enable = mkEnableOption "Squeezelite, a software Squeezebox emulator";
+
+    package = mkPackageOption pkgs "Squeezelite" { default = [ "squeezelite" ]; };
+
+    finalPackage = mkPackageOption pkgs "configured Squeezelite" { default = null; } // {
+      readOnly = true;
+    };
 
     pulseAudio = mkEnableOption "pulseaudio support";
 
@@ -46,20 +46,34 @@ in
 
   ###### implementation
 
-  config = mkIf cfg.enable {
-    systemd.services.squeezelite = {
-      wantedBy = [ "multi-user.target" ];
-      after = [
-        "network.target"
-        "sound.target"
-      ];
-      description = "Software Squeezebox emulator";
-      serviceConfig = {
-        DynamicUser = true;
-        ExecStart = "${bin} -N ${dataDir}/player-name ${cfg.extraArguments}";
-        StateDirectory = builtins.baseNameOf dataDir;
-        SupplementaryGroups = "audio";
+  config = mkMerge [
+    {
+      services.squeezelite.finalPackage =
+        if cfg.pulseAudio then
+          cfg.package.override {
+            audioBackend = "pulse";
+            pulseSupport = null;
+          }
+        else
+          cfg.package;
+    }
+    (mkIf cfg.enable {
+      environment.systemPackages = [ cfg.finalPackage ];
+
+      systemd.services.squeezelite = {
+        wantedBy = [ "multi-user.target" ];
+        after = [
+          "network.target"
+          "sound.target"
+        ];
+        description = "Software Squeezebox emulator";
+        serviceConfig = {
+          DynamicUser = true;
+          ExecStart = "${bin} -N ${dataDir}/player-name ${cfg.extraArguments}";
+          StateDirectory = builtins.baseNameOf dataDir;
+          SupplementaryGroups = "audio";
+        };
       };
-    };
-  };
+    })
+  ];
 }
