4888906f2fd8aee26c8fc416d58ccacc67b7ce49
Dusk Banks <me@bb010g.com>
2024-07-07 16:09:08 -0700

    nixos/kdeconnect: fix `enable` option description
    Topic: nixos/programs.kdeConnect
    Branches: master
    Testing-branches: nixos-unstable, master
    Uploader: bb010g

--- nixos/modules/programs/kdeconnect.nix
+++ nixos/modules/programs/kdeconnect.nix
@@ -2,14 +2,13 @@
 {
   options.programs.kdeconnect = {
     enable = lib.mkEnableOption ''
-      kdeconnect.
+      KDE Connect.
 
       Note that it will open the TCP and UDP port from
       1714 to 1764 as they are needed for it to function properly.
       You can use the {option}`package` to use
       `gnomeExtensions.gsconnect` as an alternative
-      implementation if you use Gnome
-    '';
+      implementation if you use Gnome'';
     package = lib.mkPackageOption pkgs [ "plasma5Packages" "kdeconnect-kde" ] {
       example = "gnomeExtensions.gsconnect";
     };
