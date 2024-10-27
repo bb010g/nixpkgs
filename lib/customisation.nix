8654355662f93a9b4d95014b3bcbc09ab7388920
Dusk Banks <me@bb010g.com>
2024-10-26 22:41:14 -0700

    fixup! lib.mergeFunctionArgs: init

--- lib/customisation.nix
+++ lib/customisation.nix
@@ -7,7 +7,6 @@ let
   inherit (lib)
     functionArgs
     isFunction
-    mergeFunctionArgs
     mirrorFunctionArgs
     toFunction
     setFunctionArgs
@@ -35,6 +34,7 @@ let
     extends
     ;
   inherit (lib.strings) levenshtein levenshteinAtMost;
+  inherit (lib.trivial) mergeFunctionArgAttrs;
 
 in
 rec {
@@ -242,7 +242,6 @@ rec {
 
     <!-- TODO: Apply "Example:" tag to the examples above -->
 
-
     # Inputs
 
     `autoArgs`
@@ -263,7 +262,8 @@ rec {
     callPackageWith :: AttrSet -> (AttrSet -> a) -> AttrSet -> AttrSet
     ```
   */
-  allPackageArgs = autoArgs: fn: args:
+  allPackageArgs =
+    autoArgs: fn: args:
     let
       f = if isFunction fn then fn else import fn;
       fargs = functionArgs f;
@@ -276,52 +276,66 @@ rec {
       # wouldn't be passed to it
       missingArgs =
         # Filter out arguments that have a default value
-        (filterAttrs (name: value: ! value)
-        # Filter out arguments that would be passed
-        (removeAttrs fargs (attrNames allArgs)));
+        (
+          filterAttrs (name: value: !value)
+            # Filter out arguments that would be passed
+            (removeAttrs fargs (attrNames allArgs))
+        );
 
       # Get a list of suggested argument names for a given missing one
-      getSuggestions = arg: pipe (autoArgs // args) [
-        attrNames
-        # Only use ones that are at most 2 edits away. While mork would work,
-        # levenshteinAtMost is only fast for 2 or less.
-        (filter (levenshteinAtMost 2 arg))
-        # Put strings with shorter distance first
-        (sortOn (levenshtein arg))
-        # Only take the first couple results
-        (take 3)
-        # Quote all entries
-        (map (x: "\"" + x + "\""))
-      ];
+      getSuggestions =
+        arg:
+        pipe (autoArgs // args) [
+          attrNames
+          # Only use ones that are at most 2 edits away. While mork would work,
+          # levenshteinAtMost is only fast for 2 or less.
+          (filter (levenshteinAtMost 2 arg))
+          # Put strings with shorter distance first
+          (sortOn (levenshtein arg))
+          # Only take the first couple results
+          (take 3)
+          # Quote all entries
+          (map (x: "\"" + x + "\""))
+        ];
 
-      prettySuggestions = suggestions:
-        if suggestions == [] then ""
-        else if length suggestions == 1 then ", did you mean ${elemAt suggestions 0}?"
-        else ", did you mean ${concatStringsSep ", " (lib.init suggestions)} or ${lib.last suggestions}?";
+      prettySuggestions =
+        suggestions:
+        if suggestions == [ ] then
+          ""
+        else if length suggestions == 1 then
+          ", did you mean ${elemAt suggestions 0}?"
+        else
+          ", did you mean ${concatStringsSep ", " (lib.init suggestions)} or ${lib.last suggestions}?";
 
-      errorForArg = arg:
+      errorForArg =
+        arg:
         let
           loc = builtins.unsafeGetAttrPos arg fargs;
           # loc' can be removed once lib/minver.nix is >2.3.4, since that includes
           # https://github.com/NixOS/nix/pull/3468 which makes loc be non-null
-          loc' = if loc != null then loc.file + ":" + toString loc.line
-            else if ! isFunction fn then
+          loc' =
+            if loc != null then
+              loc.file + ":" + toString loc.line
+            else if !isFunction fn then
               toString fn + optionalString (pathIsDirectory fn) "/default.nix"
-            else "<unknown location>";
-        in "Function called without required argument \"${arg}\" at "
+            else
+              "<unknown location>";
+        in
+        "Function called without required argument \"${arg}\" at "
         + "${loc'}${prettySuggestions (getSuggestions arg)}";
 
       # Only show the error for the first missing argument
       error = errorForArg (head (attrNames missingArgs));
 
-    in if missingArgs == {}
-       then makeOverridable f allArgs
-       # This needs to be an abort so it can't be caught with `builtins.tryEval`,
-       # which is used by nix-env and ofborg to filter out packages that don't evaluate.
-       # This way we're forced to fix such errors in Nixpkgs,
-       # which is especially relevant with allowAliases = false
-       else abort "lib.customisation.callPackageWith: ${error}";
-
+    in
+    if missingArgs == { } then
+      makeOverridable f allArgs
+    # This needs to be an abort so it can't be caught with `builtins.tryEval`,
+    # which is used by nix-env and ofborg to filter out packages that don't evaluate.
+    # This way we're forced to fix such errors in Nixpkgs,
+    # which is especially relevant with allowAliases = false
+    else
+      abort "lib.customisation.callPackageWith: ${error}";
 
   /**
     Call the package function in the file `fn` with the required
