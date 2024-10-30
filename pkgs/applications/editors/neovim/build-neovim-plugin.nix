1afb8c7fae5bf2292e948ebeedd88b1596c1db4a
Dusk Banks <me@bb010g.com>
2024-10-30 01:33:06 -0700

    buildNeovimPlugin: format

--- pkgs/applications/editors/neovim/build-neovim-plugin.nix
+++ pkgs/applications/editors/neovim/build-neovim-plugin.nix
@@ -1,25 +1,29 @@
-{ lib
-, stdenv
-, lua
-, toVimPlugin
+{
+  lib,
+  stdenv,
+  lua,
+  toVimPlugin,
 }:
 let
   # sanitizeDerivationName
   normalizeName = lib.replaceStrings [ "." ] [ "-" ];
 
   buildNeovimPlugin =
-  # function to create vim plugin from lua packages that are already packaged in
-  # luaPackages
-  {
-    # the lua derivation to convert into a neovim plugin
-     luaAttr ? (lua.pkgs.${normalizeName attrs.pname})
-    , ...
-  }@attrs:
+    # function to create vim plugin from lua packages that are already packaged in
+    # luaPackages
+    {
+      # the lua derivation to convert into a neovim plugin
+      luaAttr ? (lua.pkgs.${normalizeName attrs.pname}),
+      ...
+    }@attrs:
     let
-      originalLuaDrv = if (lib.typeOf luaAttr == "string") then
-        lib.warn "luaAttr as string is deprecated since September 2024. Pass a lua derivation directly ( e.g., `buildNeovimPlugin { luaAttr = lua.pkgs.plenary-nvim; }`)" lua.pkgs.${normalizeName luaAttr}
-        else luaAttr;
-
+      originalLuaDrv =
+        if (lib.typeOf luaAttr == "string") then
+          lib.warn
+            "luaAttr as string is deprecated since September 2024. Pass a lua derivation directly ( e.g., `buildNeovimPlugin { luaAttr = lua.pkgs.plenary-nvim; }`)"
+            lua.pkgs.${normalizeName luaAttr}
+        else
+          luaAttr;
 
       luaDrv = originalLuaDrv.overrideAttrs (oa: {
         version = attrs.version or oa.version;
@@ -31,14 +35,20 @@ let
         '';
       });
 
-      finalDrv = toVimPlugin (luaDrv.overrideAttrs(oa: attrs // {
-          nativeBuildInputs = oa.nativeBuildInputs or [] ++ [
-            lua.pkgs.luarocksMoveDataFolder
-          ];
-          version = "${originalLuaDrv.version}-unstable-${oa.version}";
-        }));
+      finalDrv = toVimPlugin (
+        luaDrv.overrideAttrs (
+          oa:
+          attrs
+          // {
+            nativeBuildInputs = oa.nativeBuildInputs or [ ] ++ [
+              lua.pkgs.luarocksMoveDataFolder
+            ];
+            version = "${originalLuaDrv.version}-unstable-${oa.version}";
+          }
+        )
+      );
     in
-      finalDrv;
+    finalDrv;
 
 in
 buildNeovimPlugin
